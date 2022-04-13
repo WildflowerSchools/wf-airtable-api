c92c2817c87570d3d61daf88ec5fd4fe9cd5b6dd
Benjamin Jaffe-Talberg <ben.talberg@gmail.com>
2022-04-13 11:03:35 -0500

    More flexible POLYGON str handling, bump veresion to v1.7.6

--- app/geocode/utils.py
+++ app/geocode/utils.py
@@ -1,3 +1,4 @@
+import re
 from typing import Union
 
 from haversine import haversine, Unit
@@ -91,10 +92,24 @@ def get_geo_area_nearest_to_place(
         geo_point = geometry.Point(place.geometry.location.lat, place.geometry.location.lng)
 
         for ga in polygon_geo_areas:
-            str_point_list = ga.fields.polygon_coordinates.strip('(').strip(')').split(",")
+            # Example polygon str format:
+            #
+            # POLYGON ((-73.9010989 40.997664, -74.3370459 41.1888957, -74.551168 41.2934439,
+            # -74.70173 41.3622667, -74.7850821 41.3274224, -75.1418139 40.9865643, -75.2108798 40.5854214,
+            # -74.9472072 40.3178983, -74.1891479 40.4517927, -74.2639676 40.4960505, -74.247494 40.5220241,
+            # -74.2502596 40.5348199, -74.2353817 40.5579361, -74.2143148 40.5591341, -74.2037494 40.5923079,
+            # -74.2011374 40.6325788, -74.1849067 40.6464003, -74.1400525 40.6422367, -74.0952643 40.6488871,
+            # -74.0560421 40.6522048, -74.0336221 40.6935376, -74.0136887 40.7624537, -73.9336322 40.8769537,
+            # -73.9010989 40.997664))
+            match = re.match(r'(?:[POLYGON]+)?(?:[\s\(]+)?([-+\d\.\s,]+)', ga.fields.polygon_coordinates)
+            if match is None:
+                continue
+
+            str_point_list = match.group(1)
+            str_point_list = str_point_list.strip().split(",")
 
             def str_point_to_geo_point(s):
-                parts = s.strip(' ').split(' ')
+                parts = s.strip().split(' ')
                 return geometry.Point(float(parts[1]), float(parts[0]))
 
             geo_point_list = list(map(str_point_to_geo_point, str_point_list))
