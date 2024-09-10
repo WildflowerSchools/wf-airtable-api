8e1e2c346504e20a9903b8e72c4918ab67d5f566
Benjamin Jaffe-Talberg <ben.talberg@gmail.com>
2024-09-09 22:06:28 -0500

    Move to table IDs

--- app/airtable/base_school_db/hubs.py
+++ app/airtable/base_school_db/hubs.py
@@ -12,6 +12,12 @@ class AirtableHubFields(BaseModel):
     # pods: Optional[list[str]] = Field(None, alias="Pod Assignments")
     schools: Optional[list[str]] = Field(None, alias="Schools")
 
+    # TODO: Switch to column IDs
+    # name: Optional[str] = Field(None, alias="fld4pkngNLoLxi4Q7")
+    # regional_site_entrepreneurs: Optional[list[str]] = Field(None, alias="fldv4tYu6xSNgzY6l")
+    # # pods: Optional[list[str]] = Field(None, alias="Pod Assignments")
+    # schools: Optional[list[str]] = Field(None, alias="fldQbdrvHly3wucXE")
+
 
 class AirtableHubResponse(AirtableResponse):
     fields: AirtableHubFields
